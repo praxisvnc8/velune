e3762599cee7f4f5921b5669292089bec1d5f56f
praxisvnc8 <praxis.vnc8@gmail.com>
2026-09-10 15:33:44 +0530

    build(android): configure Gradle repositories and build settings for emulator execution

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -14,13 +14,14 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://storage.flutter-io.cn/download.flutter.io") }
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "9.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.0" apply false
+    id("com.android.application") version "8.12.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
